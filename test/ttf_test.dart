958c187aa748f43d189c31f4652c43ed46103b83
westracer <westracer1@gmail.com>
2020-06-27 23:30:53 +0700

    * Header table encoding test data change

--- test/ttf_test.dart
+++ test/ttf_test.dart
@@ -338,11 +338,11 @@ void main() {
     });
 
     test('Header table', () {
-      const expected = 'AAEAAAABAAC1ZDInXw889QALBAQAAAAA2lveGAAAAADaW94Y//X/ZwZkA1UAAAAIAAIAAAAA';
+      const expected = 'AAEAAAABAABPp3pFXw889QALBAQAAAAA2lveGAAAAADaW94Y//X/ZwZkA1UAAAAIAAIAAAAA';
       final actual = base64Encode(recreatedByteData.buffer.asUint8List(recreatedFont.head.entry.offset, recreatedFont.head.entry.length));
 
       expect(actual, expected);
-      expect(recreatedFont.head.entry.checkSum, 3482595860);
+      expect(recreatedFont.head.entry.checkSum, 439353325);
     });
 
     test('Glyph Substitution table', () {
