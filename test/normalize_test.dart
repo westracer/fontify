a0826536710b9b4556eec85764c23e441eba0f99
westracer <westracer1@gmail.com>
2020-09-04 00:01:07 +0700

    Added tests for metrics depending on normalization setting

---
+++ test/normalize_test.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:fontify/src/common/generic_glyph.dart';
+import 'package:fontify/src/otf/defaults.dart';
+import 'package:fontify/src/otf/otf.dart';
+import 'package:fontify/src/svg/svg.dart';
+import 'package:test/test.dart';
+
+import 'constant.dart';
+
+const _kTestCompSvgPathList = [
+  '$kTestAssetsDir/svg/comp_first.svg',
+  '$kTestAssetsDir/svg/comp_second.svg',
+  '$kTestAssetsDir/svg/comp_third.svg',
+];
+
+void main() {
+  group('Normalization', () {    
+    List<GenericGlyph> _createGlyphList() {
+      final svgFileList = _kTestCompSvgPathList.map((e) => File(e));
+      final svgList = svgFileList.map((e) => Svg.parse(e.path, e.readAsStringSync()));
+      return svgList.map((e) => GenericGlyph.fromSvg(e)).toList();
+    }
+
+    test('Metrics, normalization is off', () {
+      final font = OpenTypeFont.createFromGlyphs(glyphList: _createGlyphList(), normalize: false);
+      final widthList = font.hmtx.hMetrics.map((e) => e.advanceWidth);
+      final unitsPerEm = kDefaultOpenTypeUnitsPerEm;
+      
+      expect(widthList, [298, 333, unitsPerEm, unitsPerEm, unitsPerEm]);
+    });
+
+    test('Metrics, normalization is on', () {
+      final font = OpenTypeFont.createFromGlyphs(glyphList: _createGlyphList(), normalize: true);
+      final widthList = font.hmtx.hMetrics.map((e) => e.advanceWidth);
+      
+      expect(widthList, [298, 333, 362, 270, 208]);
+    });
+  });
+}
--- test/test_all.dart
+++ test/test_all.dart
@@ -1,6 +1,7 @@
 import 'package:test/test.dart';
 
 import 'cli_test.dart' as cli;
+import 'normalize_test.dart' as norm;
 import 'svg_test.dart' as svg;
 import 'ttf_test.dart' as ttf;
 
@@ -8,4 +9,5 @@ void main() {
   group('TTF', ttf.main);
   group('SVG', svg.main);
   group('CLI', cli.main);
+  group('Normalization', norm.main);
 }
